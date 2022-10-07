17461b9b2d0601a958389b4ca351796b92054396
Lukasz Kolosowski <lkkolosowski@gmail.com>
2022-10-07 15:04:54 +0200

    the code has been more organized

--- js/script.js
+++ js/script.js
@@ -1,18 +1,26 @@
 {
-  console.log("Cześć! Ten kod jest już w repozytorium Git.");
-
-  const body = document.querySelector(".body");
-  const changeThemeButton = document.querySelector(".js-button");
-  const changeText = document.querySelector(".js-text");
-  const image = document.querySelector(".js-image");
-
+  const sayHello = () => {
+    console.log("Cześć! Ten kod jest już w repozytorium Git.");
+  };
+  
   const imageChange = () => {
+    const image = document.querySelector(".js-image");
+    const arrows = document.querySelectorAll(".js-arrow");
+    arrows.forEach((arrow) => {
+      arrow.addEventListener("click", () => {
+        image.src = image.src.includes("lukasz.jpg") ? "images/tymoteusz.jpg" : "images/lukasz.jpg";
+      });
+    });
+
     image.addEventListener("click", () => {
       image.src = image.src.includes("lukasz.jpg") ? "images/tymoteusz.jpg" : "images/lukasz.jpg";
     });
   };
 
   const themeChange = () => {
+    const body = document.querySelector(".body");
+    const changeThemeButton = document.querySelector(".js-button");
+    const changeText = document.querySelector(".js-text");
     changeThemeButton.addEventListener("click", () => {
       body.classList.toggle("js-dark-theme");
 
@@ -23,6 +31,7 @@
   const init = () => {
     imageChange();
     themeChange();
+    sayHello();
   };
 
   init();
